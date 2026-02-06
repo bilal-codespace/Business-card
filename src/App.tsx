8d12c67b53c73c6b6c0afe51cf48d483953a9418
Mohammad Bilal <mohdbilal4291@gmail.com>
2026-02-06 21:13:33 +0530

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-
-const queryClient = new QueryClient();
+import BusinessCard from "@/components/BusinessCard";
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <div className="flex min-h-screen items-center justify-center bg-muted/50 p-8">
+    <BusinessCard />
+  </div>
 );
 
 export default App;
